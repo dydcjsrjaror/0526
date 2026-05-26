31fadc7d8addfcd0445f9b0749667ab1ac75f49f
dydcjsrjaror <jangst15@gmail.com>
2026-05-26 13:28:56 +0900

    fix: test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send(`<h1>������ 2026년 대기업 인프라 엔지니어 이직 성공!</h1>
+    res.send(`<h1>������ 2026년 인프라 엔지니어 !</h1>
               <p>완전 자동화 GitOps(GitHub Actions + ArgoCD) 파이프라인 구동 중입니다.</p>
               <p>현재 응답하는 인프라 Pod: <b style="color: blue;">${process.env.HOSTNAME}</b></p>`);
 });
